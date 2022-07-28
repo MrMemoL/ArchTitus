9d262f55f443f2cd3c88594c78ee08342c24c841
MrMemoL <61466183+MrMemoL@users.noreply.github.com>
2022-07-28 15:08:59 +0300

    Update 0-preinstall.sh

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -36,13 +36,6 @@ pacman -S --noconfirm --needed reflector rsync grub
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo -ne "
 -------------------------------------------------------------------------
-                    Setting up $iso mirrors for faster downloads
--------------------------------------------------------------------------
-"
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-mkdir /mnt &>/dev/null # Hiding error message if any
-echo -ne "
--------------------------------------------------------------------------
                     Installing Prerequisites
 -------------------------------------------------------------------------
 "
